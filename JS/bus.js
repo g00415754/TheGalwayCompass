8a5e0aa902524aa498c3353c146bd4999bf3169b
g00415754 <dianakudrjavceva13@gmail.com>
2025-03-21 21:15:11 +0000

    homepage added gallery and tours; history page made

--- JS/bus.js
+++ JS/bus.js
@@ -1,14 +1,24 @@
 gsap.registerPlugin(ScrollTrigger);
 
-    gsap.timeline({
-        scrollTrigger: {
-            trigger: "#bus-section",
-            start: "top 80%", 
-            end: "center center", 
-            scrub: 3,
-        }
-    })
+gsap.timeline({
+    scrollTrigger: {
+        trigger: "#bus-section",
+        start: "top 80%",
+        end: "center center",
+        scrub: 3,
+    }
+})
     .fromTo(".bus-container",
         { x: "-20vw", y: "10vh", scale: 0.3 },
         { x: "10vw", y: "0vh", scale: 1.3, duration: 3, ease: "power2.out" }
     );
+
+gsap.to(".character", {
+    x: "80vw",
+    scrollTrigger: {
+        trigger: "#tours",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true
+    }
+});
